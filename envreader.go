712e61bdeeab3ed3eeea1867d631f7ac2478c63d
Michael Wolf <maw@pobox.com>
2023-09-14 12:50:18 -0600

    Add first cut at program to load and then spit out contents of .env files

---
+++ envreader.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/alessio/shellescape"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	var env map[string]string
+	flag.Parse()
+
+	env, err := godotenv.Read()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	args := flag.Args()
+
+	if len(args) > 0 {
+		for _, k := range args {
+			fmt.Printf("%s=%s\n", k, shellescape.Quote(env[k]))
+		}
+	} else {
+
+		for k, v := range env {
+			fmt.Printf("%s=%s\n", k, shellescape.Quote(v))
+		}
+	}
+}
