8977f424d55b3026a785fde6a84f08ba60576a25
Michael Wolf <maw@pobox.com>
2023-09-22 12:33:40 -0600

    Implement capture mode

--- envare.go
+++ envare.go
@@ -6,10 +6,15 @@ import (
 	"github.com/alessio/shellescape"
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
 	var env map[string]string
+
+	var captureMode bool
+
+	flag.BoolVar(&captureMode, "c", false, "run in capture-for-eval mode")
 	flag.Parse()
 
 	env, err := godotenv.Read()
@@ -19,15 +24,22 @@ func main() {
 
 	args := flag.Args()
 
-	if len(args) == 1 {
-		fmt.Printf("%s", shellescape.Quote(env[args[0]]))
-	} else if len(args) > 1 {
-		for _, k := range args {
-			fmt.Printf("%s=%s\n", k, shellescape.Quote(env[k]))
-		}
-	} else {
+	if (len(args)) > 1 {
+		captureMode = true
+	}
+	if len(args) == 0 {
 		for k, v := range env {
 			fmt.Printf("%s=%s\n", k, shellescape.Quote(v))
 		}
+		os.Exit(0)
+	}
+
+	if len(args) == 1 && captureMode == false {
+		fmt.Printf("%s", shellescape.Quote(env[args[0]]))
+		os.Exit(0)
+	}
+
+	for _, k := range args {
+		fmt.Printf("%s=%s\n", k, shellescape.Quote(env[k]))
 	}
 }
